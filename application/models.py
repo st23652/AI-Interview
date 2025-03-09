55bf6b3ad94105244dd1169a6ce63ac4614c0b36
Sneha Tandon <st23652@essex.ac.uk>
2025-03-09 17:19:07 +0000

    modifing models.py

--- application/models.py
+++ application/models.py
@@ -30,16 +30,16 @@ class Interview(models.Model):
         return self.title
 
 class InterviewQuestion(models.Model):
-    interview = models.ForeignKey(Interview, related_name='questions', on_delete=models.CASCADE)
+    interview = models.ForeignKey('Interview', related_name='questions', on_delete=models.CASCADE)
     question_text = models.TextField()
     order = models.PositiveIntegerField()
-    
+
     def __str__(self):
         return f'Question {self.order}: {self.question_text[:50]}'
 
 class InterviewResponse(models.Model):
-    interview = models.ForeignKey(Interview, related_name='responses', on_delete=models.CASCADE)
-    question = models.ForeignKey(InterviewQuestion, related_name='responses', on_delete=models.CASCADE)
+    interview = models.ForeignKey('Interview', related_name='responses', on_delete=models.CASCADE)
+    question = models.ForeignKey('InterviewQuestion', related_name='responses', on_delete=models.CASCADE)
     response_text = models.TextField()
 
     def __str__(self):
