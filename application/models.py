82facbedb0742d9b5d575daa45769f9fbc7f8376
Sneha Tandon <st23652@essex.ac.uk>
2025-03-10 01:13:45 +0000

    fixing imports in the models.py

--- application/models.py
+++ application/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import BaseUserManager, AbstractUser
-from . import models
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import settings
