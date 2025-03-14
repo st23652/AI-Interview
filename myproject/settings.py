bb366b22e41d73c158537908b5d85c55a9f8f21b
Sneha Tandon <st23652@essex.ac.uk>
2025-03-14 21:57:04 +0000

    updating the allowed hosts as starting proxy server

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'b09a-2001-8f8-1a61-c9ac-b966-ccbc-33cc-9bbf.ngrok-free.app'
+    '0905-82-132-221-154.ngrok-free.app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
